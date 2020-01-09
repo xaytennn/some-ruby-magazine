76ee634afe2ba946fc300b11473325430ee5f107
Renat <58852727+awuce@users.noreply.github.com>
2020-01-09 15:56:36 +0700

    Products pages begin

---
+++ app/controllers/product_controller.rb
@@ -0,0 +1,33 @@
+class ProductController < ApplicationController
+
+  after_action :register_visit, only: [:show]
+  
+  def show
+    @product = Product.find(params[:id])
+    set_page_options
+  end
+
+  helper_method :recent_products
+  attr_accessor :product
+
+  def recent_products
+    [] if recently.none?
+    Product.where(id: recently)
+  end
+
+  def recently
+    session[:viewed_products] ||= []
+  end
+
+  def register_visit
+    session[:viewed_products] ||= []
+    session[:viewed_products] = ([@product.id] + session[:viewed_products])
+                                .unic
+                                .take(3)
+  end
+
+  def set_page_options
+    set_meta_tags product.slice(:title, :keywords, :description)
+    add_breadcrumb name 'Home', path :root_path, title: 'Home'
+  end
+end
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,28 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+Rails.application.config.assets.precompile += %w[bootstrap.scss]
+Rails.application.config.assets.precompile += %w[style.scss]
+Rails.application.config.assets.precompile += %w[memenu.scss]
+Rails.application.config.assets.precompile += %w[flexslider.scss]
+Rails.application.config.assets.precompile += %w[mystyle.scss]
+
+Rails.application.config.assets.precompile += %w[jquery-1.11.0.min.js]
+Rails.application.config.assets.precompile += %w[simpleCart.min.js]
+Rails.application.config.assets.precompile += %w[memenu.js]
+Rails.application.config.assets.precompile += %w[jquery.easydropdown.js]
+Rails.application.config.assets.precompile += %w[responsiveslides.min.js]
+Rails.application.config.assets.precompile += %w[imagezoom.js]
+Rails.application.config.assets.precompile += %w[jquery.flexslider.js]
+Rails.application.config.assets.precompile += %w[bootstrap.min.js]
+Rails.application.config.assets.precompile += %w[glyphicons-halflings.eot]
+Rails.application.config.assets.precompile += %w[glyphicons-halflings.ttf]
+Rails.application.config.assets.precompile += %w[glyphicons-halflings.woff]
+Rails.application.config.assets.precompile += %w[glyphicons-halflings.woff2]
+Rails.application.config.assets.precompile += %w[glyphicons-halflings.svg]
+Rails.application.config.assets.precompile += %w[typeahead.bundle.js]
+Rails.application.config.assets.precompile += %w[easydrop.js]
+Rails.application.config.assets.precompile += %w[mem.js]
+Rails.application.config.assets.precompile += %w[responsive.js]
+Rails.application.config.assets.precompile += %w[flexs.js]
