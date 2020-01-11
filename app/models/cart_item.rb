1d2e12b6fef13a5963edcc2cf87e8f9aeea05331
Renat <58852727+awuce@users.noreply.github.com>
2020-01-11 17:26:43 +0700

    Add migrations, specs and models (cart, cart_item, user)

---
+++ app/models/cart_item.rb
@@ -0,0 +1,5 @@
+class CartItem < ApplicationRecord
+  belongs_to :product
+  belongs_to :cart
+  validates :cart_id, :quantity, presence: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_10_144147) do
+ActiveRecord::Schema.define(version: 2020_01_11_100304) do
 
   create_table "brands", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,21 @@ ActiveRecord::Schema.define(version: 2020_01_10_144147) do
     t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "cart_items", force: :cascade do |t|
+    t.integer "cart_id"
+    t.integer "product_id"
+    t.integer "quantity"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_carts_on_user_id"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -57,6 +72,12 @@ ActiveRecord::Schema.define(version: 2020_01_10_144147) do
   create_table "related_products", id: false, force: :cascade do |t|
     t.integer "product_id"
     t.integer "related_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.boolean "admin"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
